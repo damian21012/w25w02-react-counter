bd870b983663c098ebb45a4e7e82940d8a5b9ca4
damian21012 <mlgk3441@naver.com>
2025-09-18 17:38:25 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+
+  return (
+    <div>
+      <h1>카운터 {count}</h1>
+      <button onClick = { () => {
+        setCount((prev: number) => prev +1)
+      }}
+      >증가
+      </button>
+    </div>
+  )
+}
+
+export default App
